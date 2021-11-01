ec525ab81f7b36b14ffa8b93f998011671749b8a
beinjamin <jauresbeinjamin@gmail.com>
2021-11-02 00:18:03 +0100

    Initialisation

---
+++ src/test/java/org/jaures/TrainingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jaures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
