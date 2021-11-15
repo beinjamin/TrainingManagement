6efc40efe20fdcd31852d7dd64587e3eb40062fe
beinjamin <jauresbeinjamin@gmail.com>
2021-11-15 18:44:44 +0100

    Construction de notreclass formation et mise en relation

--- src/main/java/org/jaures/Formation.java
+++ src/main/java/org/jaures/Formation.java
@@ -1,7 +1,22 @@
 package org.jaures;
 
-public class Formation {
+import javax.persistence.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Formation {
+	private Long id;
+	private String nom;
+	private int duree;
+	
+    
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
